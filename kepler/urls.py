cdd9137d9e5975ee6b87c7fa428e97b29a9eaf02
Max Peterson <max@incuna.com>
2012-04-21 17:24:20 +0100

    Added (temporary) redirect from the root.

--- kepler/urls.py
+++ kepler/urls.py
@@ -1,9 +1,10 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
 from django.conf.urls.static import static
+from django.core.urlresolvers import reverse
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.views.generic import TemplateView
+from django.views.generic import RedirectView#, TemplateView
 
 admin.autodiscover()
 
@@ -14,7 +15,8 @@ urlpatterns = patterns(
 
 urlpatterns += patterns('',
     # Preview page changes
-    url(r'^$', TemplateView.as_view(template_name='home.html'), name='index'),
+    #url(r'^$', TemplateView.as_view(template_name='home.html'), name='index'),
+    url(r'^$', RedirectView.as_view(url='/systems/'), name='index'),
     url(r'', include('celestial.urls')),
 )
 
