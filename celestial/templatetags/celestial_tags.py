e3a9ff68f268a47b0c0dac16fc09beb8d0494b64
James Smith <james@floppy.org.uk>
2012-04-21 22:31:54 +0100

    Render size of star

--- celestial/templatetags/celestial_tags.py
+++ celestial/templatetags/celestial_tags.py
@@ -8,5 +8,9 @@ def right_ascension(ra, max):
 def declination(dec, max):
     return (float(dec)-90.0) / -180.0 * max
 
+def stellar_radius(radius):
+    return (float(radius)*1000)
+
 register.filter('right_ascension', right_ascension)
 register.filter('declination', declination)
+register.filter('stellar_radius', stellar_radius)
