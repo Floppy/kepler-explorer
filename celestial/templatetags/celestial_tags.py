0cc56b2af48e022160464176934455171126ffdc
guygriffiths <guy.griffiths@juggler.net>
2012-04-22 14:40:46 +0100

    Added comparison of weight to cows/steaks/babies

--- celestial/templatetags/celestial_tags.py
+++ celestial/templatetags/celestial_tags.py
@@ -1,10 +1,36 @@
 from django import template
+from math import pi, sin, cos
+from decimal import Decimal
 
 register = template.Library()
 
 def right_ascension(ra, max):
     return float(ra) / 24.0 * max
-    
+
+@register.simple_tag
+def xpos(ra, dec, max):
+    r, theta = get_r_theta(ra, dec)
+    if(dec >= 0):
+        offset = 0.25
+    else:
+        offset = 0.75
+    return Decimal(max)*(Decimal(offset) + Decimal(0.25)*r*Decimal(sin(theta)))
+
+
+@register.simple_tag
+def ypos(ra, dec, max):
+    r, theta = get_r_theta(ra, dec)
+    offset = 0.5
+    return Decimal(max)*(Decimal(offset) + Decimal(0.5)*r*Decimal(cos(theta)))
+
+def get_r_theta(ra, dec):
+    if(dec > 0):
+        theta = Decimal(pi) - ra * Decimal(2*pi / 24.0)
+    else:
+        theta = Decimal(pi) + ra * Decimal(2*pi / 24.0)
+    r = (Decimal(90) - abs(dec)) / Decimal(90.0)
+    return [r, theta]
+
 def declination(dec, max):
     return (float(dec)-90.0) / -180.0 * max
 
